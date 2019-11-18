36ff57d4a3330a370eafcd02a0766579361917d6
SimonLab <simon.labondance@gmail.com>
2019-11-18 14:24:43 +0000

    define package in mix.exs, #2

---
+++ .elixir_ls/build/test/lib/elixir_auth_google/ebin/elixir_auth_google.app
@@ -0,0 +1,6 @@
+{application,elixir_auth_google,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"Minimalist Google OAuth Authentication for Elixir Apps"},
+              {modules,['Elixir.ElixirAuthGoogle']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
--- mix.exs
+++ mix.exs
@@ -1,13 +1,18 @@
 defmodule ElixirAuthGoogle.MixProject do
   use Mix.Project
 
+  @description "Minimalist Google OAuth Authentication for Elixir Apps"
+  @version "0.1.0"
+
   def project do
     [
       app: :elixir_auth_google,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: @description,
+      package: package()
     ]
   end
 
@@ -23,6 +28,15 @@ defmodule ElixirAuthGoogle.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["dwyl"],
+      licenses: ["GNU GPL v2.0"],
+      links: %{github: "https://github.com/dwyl/elixir-auth-google"},
+      files: ~w(lib LICENSE mix.exs README.md .formatter.exs)
     ]
   end
 end
