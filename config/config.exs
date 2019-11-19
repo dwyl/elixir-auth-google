93028e62380d952cb5b5a691289674023bd756c2
SimonLab <simon.labondance@gmail.com>
2019-11-19 11:27:17 +0000

    Use System.fetch_env! and reference issue, #2

--- config/config.exs
+++ config/config.exs
@@ -1,4 +1,9 @@
 use Mix.Config
+
+# Google credentials
+config :elixir_auth_google,
+  google_client_id: System.fetch_env!("GOOGLE_CLIENT_ID") #see issue #4
+
 if Mix.env() == :test do
   # define configuration specific to test environment
   # It can be useful when testing/mocking
