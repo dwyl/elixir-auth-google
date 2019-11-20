b8e2e4282520bb589fbad517ffb326d706fb7cc3
SimonLab <simon.labondance@gmail.com>
2019-11-20 09:31:02 +0000

    add debug logs for user information, #7

--- lib/elixir_auth_google.ex
+++ lib/elixir_auth_google.ex
@@ -4,7 +4,7 @@ defmodule ElixirAuthGoogle do
   """
   @google_auth_url "https://accounts.google.com/o/oauth2/v2/auth?response_type=code"
   @google_token_url "https://oauth2.googleapis.com/token"
-  @google_user_profile "https://www.googleapis.com/auth/userinfo.profile"
+  @google_user_profile "https://www.googleapis.com/oauth2/v3/userinfo"
 
   def generate_oauth_url do
     client_id = Application.get_env(:elixir_auth_google, :google_client_id)
@@ -30,7 +30,9 @@ defmodule ElixirAuthGoogle do
 
   def get_user_profile(token) do
     "#{@google_user_profile}?access_token=#{token}"
+    |> IO.inspect()
     |> HTTPoison.get!()
+    |> IO.inspect()
     |> Map.fetch!(:body)
     |> Poison.decode!()
   end
