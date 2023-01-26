f5725ef88a363727cf30d229d01c0007b486f4a0
nelsonic <nelson+github@dwyl.com>
2023-01-26 00:21:28 +0000

    bump version to 1.6.5 to release custom query params https://github.com/dwyl/elixir-auth-google/pull/78

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ElixirAuthGoogle.MixProject do
   use Mix.Project
 
   @description "Minimalist Google OAuth Authentication for Elixir Apps"
-  @version "1.6.4"
+  @version "1.6.5"
 
   def project do
     [
