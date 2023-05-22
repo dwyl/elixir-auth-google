9bf07ac56eeb24dc87bf397695fb067aac1cea60
nelsonic <nelson+github@dwyl.com>
2023-05-22 17:45:33 +0100

    bump version 1.6.8 -> 1.6.9 to release typespec see: https://github.com/dwyl/elixir-auth-google/pull/97 #96

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ElixirAuthGoogle.MixProject do
   use Mix.Project
 
   @description "Minimalist Google OAuth Authentication for Elixir Apps"
-  @version "1.6.8"
+  @version "1.6.9"
 
   def project do
     [
